357c16016b2a8d019a55ed0fef5ac78eb008230a
Abhinav Yadav <actcrick02@gmail.com>
2022-07-25 17:36:08 +0530

    port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000; // later put it into an env
+const PORT = process.env.PORT || 5000; // later put it into an env
 
 // database part
 mongoose.connect(
